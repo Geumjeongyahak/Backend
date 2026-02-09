7172ef6a656ef80570b67c0502e1949594e092f5
SwallowLee <lmh2157@outlook.kr>
2026-02-09 21:47:24 +0900

    refactor: api 패키지 제거 및 domain 폴더로 이관

--- src/main/java/sonmoeum/api/v1/auth/dto/response/LoginResponse.java
+++
@@ -1,8 +0,0 @@
-package sonmoeum.api.v1.auth.dto.response;
-
-import sonmoeum.api.v1.users.dto.response.UserResponse;
-
-public record LoginResponse(
-    String accessToken,
-    UserResponse user
-) {}
