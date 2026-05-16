215e5a5bb8fe393349be28657446697db669a4eb
leeSwallow <lmh2157@outlook.kr>
2026-05-16 20:11:07 +0900

    feat(notification): Push 구독 스키마 추가

---
+++ src/main/java/geumjeongyahak/domain/notification/enums/PushDeviceType.java
@@ -0,0 +1,7 @@
+package geumjeongyahak.domain.notification.enums;
+
+public enum PushDeviceType {
+    WEB,
+    ANDROID,
+    IOS
+}
---
+++ src/main/java/geumjeongyahak/domain/notification/repository/PushSubscriptionRepository.java
@@ -0,0 +1,14 @@
+package geumjeongyahak.domain.notification.repository;
+
+import geumjeongyahak.domain.notification.entity.PushSubscription;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PushSubscriptionRepository extends JpaRepository<PushSubscription, Long> {
+
+    Optional<PushSubscription> findByToken(String token);
+
+    List<PushSubscription> findAllByUserIdAndActiveTrue(Long userId);
+}
